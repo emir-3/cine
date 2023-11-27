a2d532bd62e4698c28974bdce95ee644d7adaaca
emir-3 <145627650+emir-3@users.noreply.github.com>
2023-11-27 08:43:43 -0600

    Agregar archivos de proyecto.

---
+++ cinee/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace cinee
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMostrarOcultar = new System.Windows.Forms.Button();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.buttonRegistrar = new System.Windows.Forms.Button();
+            this.textBoxContraseña = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxUsuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnMostrarOcultar
+            // 
+            this.btnMostrarOcultar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnMostrarOcultar.Location = new System.Drawing.Point(320, 232);
+            this.btnMostrarOcultar.Name = "btnMostrarOcultar";
+            this.btnMostrarOcultar.Size = new System.Drawing.Size(47, 42);
+            this.btnMostrarOcultar.TabIndex = 16;
+            this.btnMostrarOcultar.UseVisualStyleBackColor = true;
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.BackColor = System.Drawing.Color.DarkBlue;
+            this.buttonLogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonLogin.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLogin.ForeColor = System.Drawing.Color.Gold;
+            this.buttonLogin.Location = new System.Drawing.Point(180, 377);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(134, 52);
+            this.buttonLogin.TabIndex = 15;
+            this.buttonLogin.Text = "Iniciar sesión";
+            this.buttonLogin.UseVisualStyleBackColor = false;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // buttonRegistrar
+            // 
+            this.buttonRegistrar.BackColor = System.Drawing.Color.DarkBlue;
+            this.buttonRegistrar.Font = new System.Drawing.Font("Cooper Black", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRegistrar.ForeColor = System.Drawing.Color.Gold;
+            this.buttonRegistrar.Location = new System.Drawing.Point(23, 309);
+            this.buttonRegistrar.Name = "buttonRegistrar";
+            this.buttonRegistrar.Size = new System.Drawing.Size(291, 31);
+            this.buttonRegistrar.TabIndex = 14;
+            this.buttonRegistrar.Text = "Aún no tienes una cuenta?, Registrate";
+            this.buttonRegistrar.UseVisualStyleBackColor = false;
+            // 
+            // textBoxContraseña
+            // 
+            this.textBoxContraseña.BackColor = System.Drawing.Color.DarkBlue;
+            this.textBoxContraseña.Font = new System.Drawing.Font("Cooper Black", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxContraseña.ForeColor = System.Drawing.Color.Gold;
+            this.textBoxContraseña.Location = new System.Drawing.Point(23, 231);
+            this.textBoxContraseña.Multiline = true;
+            this.textBoxContraseña.Name = "textBoxContraseña";
+            this.textBoxContraseña.PasswordChar = '*';
+            this.textBoxContraseña.Size = new System.Drawing.Size(291, 43);
+            this.textBoxContraseña.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Gold;
+            this.label3.Location = new System.Drawing.Point(30, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 21);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Contraseña";
+            // 
+            // textBoxUsuario
+            // 
+            this.textBoxUsuario.BackColor = System.Drawing.Color.DarkBlue;
+            this.textBoxUsuario.Font = new System.Drawing.Font("Cooper Black", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUsuario.ForeColor = System.Drawing.Color.Gold;
+            this.textBoxUsuario.Location = new System.Drawing.Point(23, 132);
+            this.textBoxUsuario.Multiline = true;
+            this.textBoxUsuario.Name = "textBoxUsuario";
+            this.textBoxUsuario.Size = new System.Drawing.Size(291, 48);
+            this.textBoxUsuario.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Gold;
+            this.label2.Location = new System.Drawing.Point(30, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 21);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Usuario";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(100, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 27);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "INICIA SESIÓN";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkBlue;
+            this.ClientSize = new System.Drawing.Size(412, 450);
+            this.Controls.Add(this.btnMostrarOcultar);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.buttonRegistrar);
+            this.Controls.Add(this.textBoxContraseña);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMostrarOcultar;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Button buttonRegistrar;
+        private System.Windows.Forms.TextBox textBoxContraseña;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxUsuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ cinee/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cinee
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonRegistrar_Click(object sender, EventArgs e)
+        {
+            Form formulario = new Form2();
+            formulario.Show();
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            string usuario = textBoxUsuario.Text;
+            string contraseña = textBoxContraseña.Text;
+
+            if (usuario == "Ramón Emir" && contraseña == "cbtis75")
+            {
+                MessageBox.Show("Has iniciado sesión como Ramón Emir");
+                Form formulario = new Form3();
+                formulario.Show();
+            }
+            else if (usuario == "Ramón Emir")
+            {
+                MessageBox.Show("Contraseña incorrecta. Por favor, inténtalo de nuevo.");
+                textBoxContraseña.Clear();
+            }
+            else if (contraseña == "cbtis75")
+            {
+                MessageBox.Show("Usuario incorrecto. Por favor, inténtalo de nuevo.");
+                textBoxUsuario.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Usuario y contraseña incorrectos. Por favor, inténtalo de nuevo.");
+                textBoxUsuario.Clear();
+                textBoxContraseña.Clear();
+            }
+        }
+
+        private void btnMostrarOcultar_Click(object sender, EventArgs e)
+        {
+            if (textBoxContraseña.PasswordChar == '\0')
+            {
+                textBoxContraseña.PasswordChar = '*';
+            }
+            else
+            {
+                textBoxContraseña.PasswordChar = '\0';
+            }
+        }
+    }
+}
